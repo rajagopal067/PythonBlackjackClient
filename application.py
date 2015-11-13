c06bab225c10f578cb7bd33d304fa2acd9696814
rajagopal067 <raj.bhojan@gmail.com>
2015-11-12 16:29:54 -0800

    updated the files

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from blackjack.blackjack_client import BlackjackClient
 from blackjack.strategy.stupid_strategy import StupidStrategy
+from blackjack.strategy.my_strategy import MyStrategy
 import config
 
 if __name__ == '__main__':
-    strategy = StupidStrategy()
+    strategy = MyStrategy()
     client = BlackjackClient('ws://{}:{}/connectPython'.format(config.host, config.port), strategy)
     try:
         client.run_forever()
